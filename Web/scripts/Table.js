50707f72c9df718cae4a12dc5d100089aaf23ee8
Никита <100412266+Niks6455@users.noreply.github.com>
2024-05-26 19:02:32 +0300

    ф

--- Web/scripts/Table.js
+++ Web/scripts/Table.js
@@ -1,40 +1,122 @@
     var SelectRowData = null;
-    const data = [
-        {
-            Id: 1,
-            FIO: 'Иванов Иван Иванович',
-            DateAppoint: "07.10.2003",
-            Phone: '123-456-7890',
-            Email: 'ivanov@example.com',
-            dateOfAdmission: "28.05.2024",
-            status: 'В обработке'
-        },
-        {
-            Id: 2,
-            FIO: 'Петрова Мария Сергеевна',
-            DateAppoint: "07.05.2024",
-            Phone: '987-654-3210',
-            Email: 'petrova@example.com',
-            dateOfAdmission: "29.05.2024",
-            status: 'Завершен'
-        }
-    ];
+    var IdSelectRow = '';
+    //Api Запросы
+    const id = sessionStorage.getItem("userID");
+    const accessToken = sessionStorage.getItem("accessToken");
+    console.log("AdminId", id);
+    console.log('accessToken', accessToken)
 
-    // Функция для добавления данных на страницу
-    function addDataToTable() {
+    
+    //вывод Имени Администратора
+    const adminNameElement = document.getElementById('AdminName');
+    adminNameElement.textContent = `${sessionStorage.getItem("userName")} ${sessionStorage.getItem("userSurname")}`;
+
+
+    fetch('http://localhost:3000/order/getOrders', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${accessToken}`,
+        },
+      })
+        .then(response => {
+          if (response.status === 200) {
+            return response.json(); 
+          } 
+        })
+        .then(data => {
+            addDataToTable(data);
+        })
+        .catch(error => {
+          alert('Ошибка: ' + error.message);
+        });
+
+    function addDataToTable(data) {
         const tbody = document.getElementById('tbody');
         data.forEach(item => {
             const row = document.createElement('tr');
-            for (const key in item) {
-                const cell = document.createElement('td');
-                cell.textContent = item[key];
-                row.appendChild(cell);
+            // Изменения в соответствии с новой структурой данных
+            const cell1 = document.createElement('td');
+            cell1.textContent = item.id;
+            row.appendChild(cell1);
+            
+            const cell2 = document.createElement('td');
+            cell2.textContent = item.uesr.name + ' ' + item.uesr.surname + ' ' + item.uesr.patronymic;
+            row.appendChild(cell2);
+            
+            const cell3 = document.createElement('td');
+            const dateTimeString = item.createdAt;
+            const dateOnly = dateTimeString.split('T')[0];  // Разбиваем строку по символу 'T' и берем первую часть (дату)
+            const parts = dateOnly.split('-');  // Разбиваем дату по символу '-'
+            const formattedDate = `${parts[2]}.${parts[1]}.${parts[0]}`;  // Форматируем дату в требуемый формат "дд мм гггг"
+            cell3.textContent = formattedDate;
+            row.appendChild(cell3);
+            
+            const cell4 = document.createElement('td');
+            cell4.textContent = item.phoneNumber;
+            row.appendChild(cell4);
+
+            const cell5 = document.createElement('td');
+            cell5.textContent = item.uesr.login;
+            row.appendChild(cell5);
+
+            const cell6 = document.createElement('td');
+            item.date === null ? cell6.textContent = "-" :  cell6.textContent = item.date;
+
+            row.appendChild(cell6);
+
+            const cell7 = document.createElement('td');
+            if(item.status == 1 ){
+                const text = "В обработке"
+                cell7.textContent = text;
+            }if(item.status == 2){
+                const text = "Подтвержден"
+                cell7.textContent = text;
+            }else if(item.status == 3){
+                const text = "Отклонен"
+                cell7.textContent = text;
+            }else if(item.status == 4){
+                const text = "Завершен"
+                cell7.textContent = text;
             }
-            row.addEventListener('click', () => {
-                setPopUp(item);
-            });
+            row.appendChild(cell7);
+            
             tbody.appendChild(row);
+            addListner()
         });
+    }
+
+    function addListner(){
+        const rows = document.querySelectorAll("tr");
+        let prevSelectedRow = null;
+        const mass = []
+        rows.forEach((row,index)=>{
+            row.addEventListener('click', () => {
+                IdSelectRow = row.children[0].textContent;
+                const item = {
+                    FIO: row.children[1].textContent,
+                    DateAppoint: row.children[2].textContent,
+                    Phone: row.children[3].textContent,
+                    Email: row.children[4].textContent,
+                    dateOfAdmission: row.children[5].textContent,
+                    status: row.children[6].textContent,
+                }
+                console.log("Выбранная заявка:", IdSelectRow)
+                setPopUp(item)
+                if (prevSelectedRow) {
+                    prevSelectedRow.style.backgroundColor = ''; 
+                }
+                row.style.backgroundColor = '#cc8ddb';
+                prevSelectedRow = row;
+            });
+            row.addEventListener('mouseover', () => {
+                row.style.border = '2px solid #7A0099'; 
+            });
+        
+            row.addEventListener('mouseout', () => {
+                row.style.border = '1px solid black'; 
+            });
+        })
     }
 
     function showPopUp(){
@@ -89,29 +171,6 @@
     // Вызываем функцию для добавления данных на страницу
     addDataToTable();
 
-const rows = document.querySelectorAll('#tbody tr');
-let prevSelectedRow = null;
-rows.forEach((row, index) => {
-    row.addEventListener('mouseover', () => {
-        row.style.border = '2px solid #7A0099'; 
-    });
-
-    row.addEventListener('mouseout', () => {
-        row.style.border = '1px solid black'; 
-    });
-
-    row.addEventListener('click', () => {
-        if (prevSelectedRow) {
-            prevSelectedRow.style.backgroundColor = ''; 
-        }
-        row.style.backgroundColor = '#cc8ddb';
-        // const rowNum = index + 1; 
-        // console.log('Номер выделенной строки:', rowNum);
-        prevSelectedRow = row;
-    });
-});
-
-
     // Add this function to your JS code
     function searchTable() {
         let input = document.querySelector("#search__input");
@@ -142,15 +201,3 @@ rows.forEach((row, index) => {
 
     // Add an event listener to your search input field
     document.querySelector("#search__input").addEventListener('keyup', searchTable);
-
-
-    //Api Запросы
-    const id = sessionStorage.getItem("userID");
-    const accessToken = sessionStorage.getItem("accessToken");
-    console.log("AdminId", id);
-    console.log('accessToken', accessToken)
-
-    
-    //вывод Имени Администратора
-    const adminNameElement = document.getElementById('AdminName');
-    adminNameElement.textContent = `${sessionStorage.getItem("userName")} ${sessionStorage.getItem("userSurname")}`;
